22f9756803e9ef7546ef4d129859d79f1fde8ed4
NjauSamuel <njaus602@gmail.com>
2025-08-08 00:57:41 +0300

    Added a new column called profile_image in the User's table as well as how to cast it in the user model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,15 +2,18 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravolt\Avatar\Facade as Avatar;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class User extends Authenticatable
+class User extends Authenticatable implements MustVerifyEmail, HasMedia
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, InteractsWithMedia;
 
     /**
      * The attributes that are mass assignable.
@@ -45,4 +48,26 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function getProfileImageUrlAttribute()
+    {
+        if ($this->hasMedia('profile')) {
+            return $this->getFirstMediaUrl('profile');
+        }        
+
+        $initials = $this->generateInitials($this->name);
+        return Avatar::create($initials)->toBase64();
+    }
+
+    protected function generateInitials($name)
+    {
+        $parts = preg_split('/\s+/', trim($name));
+
+        if (count($parts) >= 2) {
+            return strtoupper(substr($parts[0], 0, 1) . substr(end($parts), 0, 1));
+        }
+
+        return strtoupper(substr($name, 0, 1));
+    }
+
 }
