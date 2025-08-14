25d9535075dad8aeb8880283a4faf6fd562352ae
NjauSamuel <njaus602@gmail.com>
2025-08-15 01:21:24 +0300

    Added name to dashboard dropdown.

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -19,7 +19,7 @@ class RegisterController extends Controller
     public function register(Request $request)
     {
         $validated = $request->validate([
-            'name' => ['required', 'string', 'max:255'],
+            'name' => ['required', 'string', 'max:25'],
             'email' => ['required', 'email', 'unique:users,email'],
             'password' => ['required', 'confirmed', 'min:8'],
         ]);
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -159,6 +159,9 @@
                                         class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700">
                                         <ul class="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownHoverButton">
                                             <li>
+                                                <span class="block pl-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white font-bold text-lg">{{ Auth::user()->name }} </span>                                                </span>
+                                            </li>
+                                            <li>
                                                 <a href="#" onclick="showAlert()" class="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
                                             </li>
                                             <li>
